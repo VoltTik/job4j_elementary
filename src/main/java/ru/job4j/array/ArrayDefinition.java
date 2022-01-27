c7f03f2dd0826c7f9a13ef9bbf6bb28537de0218
s.chirkov <s.chirkov@cdek.ru>
2022-01-27 23:47:19 +0300

    6.0.2. Заполнение массива

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages: " + ages.length);
         System.out.println("Размер массива surnames: " + surnames.length);
         System.out.println("Размер массива prices: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Stephen";
+        names[1] = "Ivan";
+        names[2] = "Petr";
+        names[3] = "Merelin";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
