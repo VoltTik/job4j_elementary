3c535439bc46d05fa4e8f92ca18d2ef4408848f7
s.chirkov <s.chirkov@cdek.ru>
2022-01-27 20:28:42 +0300

    5.7. Ипотека

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public static int year(double amount, int salary, double percent){
+        int year = 0;
+        int earnedSum = 0;
+        double sumToPay = amount;
+        while (earnedSum < sumToPay){
+            year += 1;
+            earnedSum += salary;
+            sumToPay += amount * percent / 100;
+
+        }
+        return year;
+    }
+}
