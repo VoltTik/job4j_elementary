ab22163fe55a1e0ed1cf0b655a53691421ba1ba6
s.chirkov <s.chirkov@cdek.ru>
2022-01-29 02:43:29 +0300

    6.3. Массив заполнен true или false

---
+++ src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        boolean[] data = new boolean[] {true, true, true};
+        boolean result = Check.mono(data);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        boolean[] data = new boolean[] {true, false, true};
+        boolean result = Check.mono(data);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void whenDataMonoByFalseThenTrue() {
+        boolean[] data = new boolean[] {false, false, false};
+        boolean result = Check.mono(data);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenDataNotMonoByFalseThenFalse() {
+        boolean[] data = new boolean[] {false, true, false};
+        boolean result = Check.mono(data);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void whenLastElementNotReference() {
+        boolean[] data = new boolean[] {false, false, false, false, true};
+        boolean result = Check.mono(data);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void whenArrayHasOneElement() {
+        boolean[] data = new boolean[] {false};
+        boolean result = Check.mono(data);
+        Assert.assertTrue(result);
+    }
+
+}
