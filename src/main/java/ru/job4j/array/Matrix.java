a4e181562d270dcdd8d3642cdaa212795edf6b3c
VoltVolt <serapite@mail.ru>
2022-01-31 21:07:28 +0300

    6.6.8. Двухмерный массив. Таблица умножения (2)

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -7,9 +7,7 @@ public class Matrix {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 array[i][j] = (i + 1) * (j + 1);
-                System.out.print(array[i][j] + "  ");
             }
-            System.out.println();
         }
         return array;
     }
